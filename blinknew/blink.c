d8dc7a87db28e7b195fbb5d0bc679a10427f0f69
Logan Sweet <logan.sweet@students.olin.edu>
2018-01-29 19:25:38 -0500

    update for links in MD

--- blinknew/blink.c
+++ blinknew/blink.c
@@ -1,84 +1,37 @@
 
-#include "elecanisms.h"
-#include <stdio.h>
-#include <time.h>
 
+
+
+
+
+
+
+/*
+Brenna's blink
+
+
+#include <p24FJ128GB206.h>
+//#include "common.h"
+//#include "ui.h"
+#include "timer.h"
 
 int16_t main(void) {
-    init_elecanisms();
+    init_clock();
+    init_ui();
+    init_timer();
 
-    T1CON = 0x0030;         // set Timer1 period to 0.5s
-    PR1 = 0x7A11;
+    led_on(&led1);
+    timer_setPeriod(&timer2, 0.5);
+    timer_start(&timer2);
 
-    TMR1 = 0;               // set Timer1 count to 0
-    IFS0bits.T1IF = 0;      // lower Timer1 interrupt flag
-    T1CONbits.TON = 1;      // turn on Timer1
-
-    time_t start_t, end_t;
-    double diff_t;
-
-    time(&start_t);
-    time(&end_t);
-    diff_t = difftime(end_t, start_t);
-
-while (diff_t < 3){
-    LED3 = ON;
-    time(&end_t);
-    diff_t = difftime(end_t, start_t);
-}
-
-while (diff_t >= 3 && diff_t >= 6){
-    LED3 = OFF;
-    time(&end_t);
-    diff_t = difftime(end_t, start_t);
-}
-
-while (diff_t > 6){
-    time(&end_t);
-    time(&end_t);
-    diff_t = difftime(end_t, start_t);
-}
-
-
-/*
     while (1) {
-        if (IFS0bits.T1IF == 1) {
-            IFS0bits.T1IF = 0;      // lower Timer1 interrupt flag
-            LED2 = !LED2;           // toggle LED2
-            //LED2 = ON;
-          }
-        //LED1 = (D0 == 0) ? OFF : ON;   // turn LED1 on if SW2 is pressed
-        //LED3 = (SW3 == 0) ? ON : OFF;   // turn LED3 on if SW3 is pressed
+        if (timer_flag(&timer2)) {
+            timer_lower(&timer2);
+            led_toggle(&led1);
+        }
+        led_write(&led2, !sw_read(&sw2));
+        led_write(&led3, !sw_read(&sw3));
     }
-    */
 }
 
-
-
-
-
-/*
-** Copyright (c) 2018, Bradley A. Minch
-** All rights reserved.
-**
-** Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are met:
-**
-**     1. Redistributions of source code must retain the above copyright
-**        notice, this list of conditions and the following disclaimer.
-**     2. Redistributions in binary form must reproduce the above copyright
-**        notice, this list of conditions and the following disclaimer in the
-**        documentation and/or other materials provided with the distribution.
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-** AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-** ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-** LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-** CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-** SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-** INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-** CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-** ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-** POSSIBILITY OF SUCH DAMAGE.
 */
