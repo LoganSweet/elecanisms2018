a005ddbefc8b27818414755c4ab85ed48fdaee97
Logan Sweet <loganrsweet@gmail.com>
2018-02-05 19:50:32 -0500

    added comments to usbtest with Brenna's help

--- usbtest/usbtest.py
+++ usbtest/usbtest.py
@@ -3,29 +3,28 @@
 ## All rights reserved.
 ##
 ## Redistribution and use in source and binary forms, with or without
-## modification, are permitted provided that the following conditions are met: 
-## 
-##     1. Redistributions of source code must retain the above copyright 
-##        notice, this list of conditions and the following disclaimer. 
-##     2. Redistributions in binary form must reproduce the above copyright 
-##        notice, this list of conditions and the following disclaimer in the 
-##        documentation and/or other materials provided with the distribution. 
+## modification, are permitted provided that the following conditions are met:
 ##
-## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-## ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
-## LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-## CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-## SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-## INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-## CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-## ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+##     1. Redistributions of source code must retain the above copyright
+##        notice, this list of conditions and the following disclaimer.
+##     2. Redistributions in binary form must reproduce the above copyright
+##        notice, this list of conditions and the following disclaimer in the
+##        documentation and/or other materials provided with the distribution.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+## ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+## LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+## CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+## SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+## INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+## CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+## ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ## POSSIBILITY OF SUCH DAMAGE.
 #
 
 import usb.core
-
 class usbtest:
 
     def __init__(self):
@@ -35,12 +34,15 @@ class usbtest:
         self.READ_SW1 = 3
         self.READ_SW2 = 4
         self.READ_SW3 = 5
-        self.READ_A0 = 6 
+        self.READ_A0 = 6
         self.SET_DUTY_VAL = 7
         self.GET_DUTY_VAL = 8
         self.GET_DUTY_MAX = 9
 
         self.dev = usb.core.find(idVendor = 0x6666, idProduct = 0x0003)
+        # looks for something in usb port
+        # self/dev is your pic
+
         if self.dev is None:
             raise ValueError('no USB device found matching idVendor = 0x6666 and idProduct = 0x0003')
         self.dev.set_configuration()
@@ -126,4 +128,3 @@ class usbtest:
 
     def get_duty(self):
         return 100. * self.get_duty_val() / self.get_duty_max()
-
