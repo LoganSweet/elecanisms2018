ba90b7a24ab248e94a56fd8c5eab4501bf6edd24
Logan Sweet <loganrsweet@gmail.com>
2018-02-06 10:02:58 -0500

    files rearranged and better named

--- miniproject1/miniproject1.c
+++ miniproject1/miniproject1.c
@@ -3,16 +3,24 @@
 #include <stdio.h>
 
 #define READ_A0             0
-#define SET_DUTY_VAL        1
-#define GET_DUTY_VAL        2
-#define GET_DUTY_MAX        3
+#define READ_A1             1
+#define SET_DUTY_VAL        2
+#define GET_DUTY_VAL        3
+#define GET_DUTY_MAX        4
 
 void vendor_requests(void) {
-    //WORD temp;
+    WORD temp;
     //uint16_t i;
     switch (USB_setup.bRequest) {
         case READ_A0:
             temp.w = read_analog(A0_AN);
+            BD[EP0IN].address[0] = temp.b[0];
+            BD[EP0IN].address[1] = temp.b[1];
+            BD[EP0IN].bytecount = 2;
+            BD[EP0IN].status = UOWN | DTS | DTSEN;
+            break;
+        case READ_A1:
+            temp.w = read_analog(A1_AN);
             BD[EP0IN].address[0] = temp.b[0];
             BD[EP0IN].address[1] = temp.b[1];
             BD[EP0IN].bytecount = 2;
@@ -46,23 +54,21 @@ int16_t main(void) {
     uint8_t *RPOR, *RPINR;
     init_elecanisms();
 
-    // Configure pin D13 to produce a 1-kHz PWM signal with a 25% duty cycle using the OC1 module. doc at http://elecanisms.olin.edu/frm/35_Output_Compare.pdf
-    D8_DIR = OUT;      // configure D13 to be a digital output
-    D8 = 0;            // set D13 low
+    D7_DIR = OUT;                                       // pin number here
+    D7 = 0;                                         // pin number
     RPOR = (uint8_t *)&RPOR0;
     RPINR = (uint8_t *)&RPINR0;
 
     __builtin_write_OSCCONL(OSCCON & 0xBF);
-    RPOR[D8_RP] = OC1_RP;  // connect the OC1 module output to pin D13
+    RPOR[D7_RP] = OC1_RP;                                          // pin number
     __builtin_write_OSCCONL(OSCCON | 0x40);
 
-    OC1CON1 = 0x1C06;   // configure OC1 module to use the peripheral clock (i.e., FCY, OCTSEL<2:0> = 0b111) and
-                        //   and to operate in edge-aligned PWM mode OCM<2:0> = 0b110)
-    OC1CON2 = 0x001F;   // configure OC1 module to syncrhonize to itself (i.e., OCTRIG = 0 and SYNCSEL<4:0> = 0b11111)
+    OC1CON1 = 0x1C06;
+    OC1CON2 = 0x001F;
 
-    OC1RS = (uint16_t)(FCY / 1e3 - 1.);     // configure period register to get a frequency of 1kHz
-    OC1R = OC1RS >> 2;  // configure duty cycle to 25% (i.e., period / 4)
-    OC1TMR = 0;         // set OC1 timer count to 0
+    OC1RS = (uint16_t)(FCY / 1e3 - 1.);
+    OC1R = OC1RS >> 2;
+    OC1TMR = 0;
 
     USB_setup_vendor_callback = vendor_requests;
     init_usb();
