310b0986cdf9c7a563d46aa9f3ec46f644f7cf7d
Logan Sweet <loganrsweet@gmail.com>
2018-02-06 09:14:54 -0500

    moved into higher directory so it can run

---
+++ test2/usbtest.c
@@ -0,0 +1,77 @@
+#include "elecanisms.h"
+#include "usb.h"
+#include <stdio.h>
+
+#define READ_A0             0
+#define SET_DUTY_VAL        1
+#define GET_DUTY_VAL        2
+#define GET_DUTY_MAX        3
+
+void vendor_requests(void) {
+    WORD temp;
+    //uint16_t i;
+    switch (USB_setup.bRequest) {
+        case READ_A0:
+            temp.w = read_analog(A0_AN);
+            BD[EP0IN].address[0] = temp.b[0];
+            BD[EP0IN].address[1] = temp.b[1];
+            BD[EP0IN].bytecount = 2;
+            BD[EP0IN].status = UOWN | DTS | DTSEN;
+            break;
+        case SET_DUTY_VAL:
+            OC1R = USB_setup.wValue.w;
+            BD[EP0IN].bytecount = 0;
+            BD[EP0IN].status = UOWN | DTS | DTSEN;
+            break;
+        case GET_DUTY_VAL:
+            temp.w = OC1R;
+            BD[EP0IN].address[0] = temp.b[0];
+            BD[EP0IN].address[1] = temp.b[1];
+            BD[EP0IN].bytecount = 2;
+            BD[EP0IN].status = UOWN | DTS | DTSEN;
+            break;
+        case GET_DUTY_MAX:
+            temp.w = OC1RS;
+            BD[EP0IN].address[0] = temp.b[0];
+            BD[EP0IN].address[1] = temp.b[1];
+            BD[EP0IN].bytecount = 2;
+            BD[EP0IN].status = UOWN | DTS | DTSEN;
+            break;
+        default:
+            USB_error_flags |= REQUEST_ERROR;
+    }
+}
+
+int16_t main(void) {
+    uint8_t *RPOR, *RPINR;
+    init_elecanisms();
+
+    D8_DIR = OUT;                                       // pin number here
+    D8 = 0;                                         // pin number
+    RPOR = (uint8_t *)&RPOR0;
+    RPINR = (uint8_t *)&RPINR0;
+
+    __builtin_write_OSCCONL(OSCCON & 0xBF);
+    RPOR[D8_RP] = OC1_RP;                                          // pin number
+    __builtin_write_OSCCONL(OSCCON | 0x40);
+    OC1CON1 = 0x1C06;
+    OC1CON2 = 0x001F;
+
+    OC1RS = (uint16_t)(FCY / 1e3 - 1.);
+    OC1R = OC1RS >> 2;
+    OC1TMR = 0;
+
+    USB_setup_vendor_callback = vendor_requests;
+    init_usb();
+
+    while (USB_USWSTAT != CONFIG_STATE) {
+#ifndef USB_INTERRUPT
+        usb_service();
+#endif
+    }
+    while (1) {
+#ifndef USB_INTERRUPT
+        usb_service();
+#endif
+    }
+}
